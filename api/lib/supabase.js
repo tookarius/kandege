a1188a008b4722711dcdd1401bc9851230a62858
TK <121897808+tookarius@users.noreply.github.com>
2026-04-05 02:30:59 +0300

    Create supabase.js

---
+++ api/lib/supabase.js
@@ -0,0 +1,9 @@
+// api/lib/supabase.js
+const { createClient } = require('@supabase/supabase-js');
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+module.exports = supabase;
